d1080cd570ece8891104444f8a48aef9083cce01
jantgg <smosh360@hotmail.com>
2022-11-23 16:42:52 +0000

    consolabien

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let com = ".com";
+  let domain = [];
+  for (let x = 0; x < 2; x++) {
+    for (let y = 0; y < 2; y++) {
+      for (let j = 0; j < 2; j++) {
+        console.log(pronoun[x] + adj[y] + noun[j] + com);
+        domain = pronoun[x] + adj[y] + noun[j] + com;
+      }
+    }
+  }
+
+  document.querySelector("#greeting").innerHTML = domain;
 };
