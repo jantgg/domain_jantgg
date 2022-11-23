49684d88bac99a49e5916e3b9561973007b93688
jantgg <smosh360@hotmail.com>
2022-11-23 16:48:45 +0000

    mas variables

--- src/app.js
+++ src/app.js
@@ -9,16 +9,17 @@ window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
-  let com = ".com";
+  let com = [".com", ".net", ".us", ".io"];
   let domain = [];
   for (let x = 0; x < 2; x++) {
     for (let y = 0; y < 2; y++) {
       for (let j = 0; j < 2; j++) {
-        console.log(pronoun[x] + adj[y] + noun[j] + com);
-        domain = pronoun[x] + adj[y] + noun[j] + com;
+        for (let z = 0; z < 4; z++) {
+          console.log(pronoun[x] + adj[y] + noun[j] + com[z]);
+          domain = pronoun[x] + adj[y] + noun[j] + com[z];
+        }
       }
     }
   }
-
   document.querySelector("#greeting").innerHTML = domain;
 };
